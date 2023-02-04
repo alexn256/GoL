da9cc9456590560392bd86ee82a8b52bf9996f5b
Alexander Naumov <anaumov@solutionmentors.com>
2023-02-04 19:48:30 +0200

    add canvas and basic styles

--- game.js
+++ game.js
@@ -1,10 +1,22 @@
 const board = [];
 
-const initBoard() => {
-    for (const i ) {
-        if (Object.hasOwnProperty.call(object, key)) {
-            const element = object[key];
-            
-        }
+let speed = document.querySelector("#volume");
+let canvas = document.querySelector(".canvas");
+
+speed.addEventListener("input", () => {
+  console.log("speed = " + speed.value);
+});
+
+function drawGrid(canvas, cell) {
+  const context = canvas.getContext("2d");
+  const width = canvas.width;
+  const height = canvas.height;
+  for (let y = 1; y <= height; y += cell) {
+    for (let x = 1; x <= width; x += cell) {
+      context.fillStyle = "#76D7C4";
+      context.fillRect(y, x, cell - 2, cell - 2);
     }
+  }
 }
+
+drawGrid(canvas, 50);
