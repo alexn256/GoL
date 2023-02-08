8d9cc5145cd14f445591d17caaf90aff69939dd6
Alexander Naumov <naumov.btm@gmail.com>
2023-02-08 23:23:51 +0200

    add temporary canvas listener

--- game.js
+++ game.js
@@ -1,24 +1,57 @@
-const board = [];
+// temp solution
+const board = [
+    [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
+    [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
+    [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
+    [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
+    [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
+    [false, false, true, false, false, false, false, false, false, false, false, false, false, false, false],
+    [false, false, true, true, false, false, false, false, false, false, false, false, false, false, false],
+    [false, false, true, false, false, false, false, false, false, false, false, false, false, false, false],
+    [false, false, false, true, false, false, false, false, false, false, false, false, false, false, false],
+    [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
+    [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
+    [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
+    [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
+    [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
+    [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false]
+];
 
 let speed = document.querySelector("#volume");
 let canvas = document.querySelector(".canvas");
 
 speed.addEventListener("input", () => {
-  console.log("speed = " + speed.value);
+    console.log("speed = " + speed.value);
+});
+
+canvas.addEventListener("click", function (event) {
+    const x = event.offsetX;
+    const y = event.offsetY;
+    console.log("Clicked at", x, y);
+    drawCell(Math.floor(x / 50), Math.floor(y / 50), canvas.getContext("2d"));
 });
 
 function drawGrid(canvas, cell) {
-  const context = canvas.getContext("2d");
-  const width = canvas.width;
-  const height = canvas.height;
-  for (let y = 1; y <= height; y += cell) {
-    for (let x = 1; x <= width; x += cell) {
-      context.fillStyle = "#76D7C4";
-      context.fillRect(y, x, cell - 2, cell - 2);
+    const context = canvas.getContext("2d");
+    const width = canvas.width;
+    const height = canvas.height;
+    for (let y = 0; y < height / cell; y += 1) {
+        for (let x = 0; x < width / cell; x += 1) {
+            if (board[y][x]) {
+                context.fillStyle = "#7678d7"
+            } else {
+                context.fillStyle = "#76D7C4";
+            }
+            context.fillRect(x * cell, y * cell, cell - 2, cell - 2);
+        }
     }
-  }
 }
 
-window.onload = (event) => {
-  drawGrid(canvas, 50);
-};
+function drawCell(x, y, context) {
+    console.log("Clicked at", x, y);
+    board[y][x] = true;
+    context.fillStyle = "#7678d7"
+    context.fillRect(x * 50, y * 50, 50 - 2, 50 - 2);
+}
+
+drawGrid(canvas, 50);
